ab20cb8f0f4ff866ab3d07170db6279eff34e427
loganrobertclemons <loganrobertclemons.com>
2022-07-15 09:22:47 -0500

    initial commit

--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/{{ .Owner }}/{{ .Repo }}/pkg/logger"
-	"github.com/{{ .Owner }}/{{ .Repo }}/pkg/plugin"
+	"github.com/loganrobertclemons/kubectl-loadsim/pkg/logger"
+	"github.com/loganrobertclemons/kubectl-loadsim/pkg/plugin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tj/go-spin"
@@ -21,7 +21,7 @@ var (
 
 func RootCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           "{{ .PluginName }}",
+		Use:           "kubectl-loadsim",
 		Short:         "",
 		Long:          `.`,
 		SilenceErrors: true,
--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/{{ .Owner }}/{{ .Repo }}/cmd/plugin/cli"
+	"github.com/loganrobertclemons/kubectl-loadsim/cmd/plugin/cli"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
 )
 
